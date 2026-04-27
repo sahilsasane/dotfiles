a05c3dbf504e12429ee354d4a5ea4134483530f2
sahil <shsasane22@gmail.com>
2026-04-27 16:54:14 +0530

    feat: enable mouse support for better pane and window control

--- zsh/rc.d/66-ghostty-title.zsh
+++ zsh/rc.d/66-ghostty-title.zsh
@@ -14,9 +14,32 @@ if [[ -n "${GHOSTTY_RESOURCES_DIR:-}" || "${TERM_PROGRAM:-}" == "ghostty" || "${
     print -Pn "\e]2;${title:q}\a"
   }
 
-  typeset -ga precmd_functions chpwd_functions
-  precmd_functions=(${precmd_functions:#_dotfiles_terminal_set_title} _dotfiles_terminal_set_title)
-  chpwd_functions=(${chpwd_functions:#_dotfiles_terminal_set_title} _dotfiles_terminal_set_title)
+  _dotfiles_ghostty_reset_mouse_modes() {
+    emulate -L zsh
 
-  _dotfiles_terminal_set_title
+    [[ -n "${GHOSTTY_RESOURCES_DIR:-}" || "${TERM_PROGRAM:-}" == "ghostty" ]] || return 0
+
+    # Some TUIs can leave mouse/alternate-scroll modes enabled, which makes
+    # wheel events reach zsh as up/down keys instead of scrolling scrollback.
+    print -Pn "\e[?1000l\e[?1002l\e[?1003l\e[?1005l\e[?1006l\e[?1015l\e[?1007l"
+  }
+
+  typeset -gi _dotfiles_terminal_hooks_ready=0
+  _dotfiles_terminal_precmd() {
+    emulate -L zsh
+
+    if (( ! _dotfiles_terminal_hooks_ready )); then
+      _dotfiles_terminal_hooks_ready=1
+      return 0
+    fi
+
+    _dotfiles_terminal_set_title
+    _dotfiles_ghostty_reset_mouse_modes
+  }
+
+  typeset -ga precmd_functions chpwd_functions
+  precmd_functions=(${precmd_functions:#_dotfiles_terminal_set_title})
+  precmd_functions=(${precmd_functions:#_dotfiles_ghostty_reset_mouse_modes})
+  precmd_functions=(${precmd_functions:#_dotfiles_terminal_precmd} _dotfiles_terminal_precmd)
+  chpwd_functions=(${chpwd_functions:#_dotfiles_terminal_set_title} _dotfiles_terminal_set_title)
 fi
