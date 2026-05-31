5a5e77ff6489c65c67898fb0d3976c27e0d5d6bd
sahil <shsasane22@gmail.com>
2026-06-01 01:23:20 +0530

    chore: some updates

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -147,9 +147,8 @@
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
   typeset -g POWERLEVEL9K_MODE=nerdfont-v3
-  # When set to `moderate`, some icons will have an extra space after them. This is meant to avoid
-  # icon overlap when using non-monospace fonts. When set to `none`, spaces are not added.
-  typeset -g POWERLEVEL9K_ICON_PADDING=none
+  # Add a little breathing room between icons and text without changing terminal font settings.
+  typeset -g POWERLEVEL9K_ICON_PADDING=moderate
 
   # Basic style options that define the overall look of your prompt. You probably don't want to
   # change them.
